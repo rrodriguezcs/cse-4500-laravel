165059e73098283d55bd0eb1ce7b62706673feb5
rrodriguezcs <90003077+rrodriguezcs@users.noreply.github.com>
2022-03-03 00:35:02 -0800

    Update calendar.blade.php

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,37 +1,19 @@
 @extends('adminlte::page')
 
-@section('title', 'Calendar')
+@section('title', 'title')
 
 @section('content_header')
-    <h1>Calendar</h1>
+    <h1>Header</h1>
 @stop
 
 @section('content')
-<div class="row">
-  <div class="col-md-12">
-    <div class="card card-primary">
-      <div class="card-body p-0">
-        <div id="calendar" class="fc fc-media-screen fc-direction-ltr fc-theme-bootstrap">
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+    <p>Content here</p>
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.css">
+    <link rel="stylesheet" href="/css/admin_custom.css">
 @stop
 
 @section('js')
-<script src="//cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.js" ></script>
-<script>
-$( document ).ready(function() {
-    var calendar = new FullCalendar.Calendar($('#calendar')[0], {
-      initialView: 'dayGridMonth',
-      events: '/events-feed'
-    });
-    calendar.render();
-})
-</script>
+    <script> console.log('Hi!'); </script>
 @stop
