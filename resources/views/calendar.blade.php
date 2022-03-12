b704db98bd748f6b31826196cd496f7f6d9800de
rrodriguezcs <90003077+rrodriguezcs@users.noreply.github.com>
2022-03-11 23:09:30 -0800

    removebutton

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -7,7 +7,6 @@
 @stop
 
 @section('content')
-<a href="{{ route('eventsfeed.create') }} " class="btn btn-primary" >Create</a>
 <div class="row">
   <div class="col-md-12">
     <div class="card card-primary">
