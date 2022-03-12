3ed27f99e6885d08d65060d2024cd3fd6f56d656
rrodriguezcs <90003077+rrodriguezcs@users.noreply.github.com>
2022-03-11 21:05:40 -0800

    createbladeedit

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -7,36 +7,10 @@
 @stop
 
 @section('content')
-<div class="card">
-  <div class="card-body">
-    <table id="table" class="table table-bordered">
-      <thead>
-        <tr>
-          <th style="width: 10px">#</th><th>Task</th><th>Progress</th><th style="width: 40px">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-
-        @foreach($todos AS $todo)
-        <tr>
-          <td>{{ $todo->id }}</td>
-          <td>{{ $todo->title }}</td>
-          <td><div class="progress progress-xs"><div class="progress-bar progress-bar-danger" style="width: {{ $todo->progress }}%"></div></div></td>
-          <td><a class="btn btn-default btn-sm" href="{{ route('todos.show',['todo'=>$todo->id]) }}">View</a></td>
-        </tr>
-        @endforeach
-
-      </tbody>
-    </table>
-  </div>
-</div>
-<a href="{{ route('todos.create') }} " class="btn btn-primary" >Create</a>
-@stop
-
-@section('js')
-<script>
-    $(document).ready(function() {
-        $('#table').DataTable();
-    } );
-</script>
+<form method="post" action="{{ route('todos.store') }}" >
+    @csrf
+    <x-adminlte-input name="title" label="Title" />
+    <x-adminlte-input name="progress" type="number" min=0 max=100 label="Progress" />
+    <x-adminlte-button type="Submit" label="Submit" />
+</form>
 @stop
